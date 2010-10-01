c43129ba9108148ea0f7d89fde414df17d99364e
TheBuzzSaw <TheBuzzSaw@15c703df-09f0-e5ca-e0ae-cfe7d5314c36>
2010-10-01 20:06:59 +0000

    added pthread to script
    git-svn-id: http://libxpg.googlecode.com/svn/trunk@13 15c703df-09f0-e5ca-e0ae-cfe7d5314c36

--- compile-test.sh
+++ compile-test.sh
@@ -1,1 +1,1 @@
-g++ -o test-xpg -O2 -Isource/XPG/glew -Isource source/XPG/glew/glew.c source/test/*.cpp source/XPG/core/*.cpp source/XPG/unix/*.cpp source/XPG/linux/*.cpp -lGL -lX11
+g++ -o test-xpg -O2 -Isource/XPG/glew -Isource source/XPG/glew/glew.c source/test/*.cpp source/XPG/core/*.cpp source/XPG/unix/*.cpp source/XPG/linux/*.cpp -lGL -lX11 -pthread
